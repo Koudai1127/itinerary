404d1b50a008b6c2234695c1b56a080014730232
Koudai1127 <koudai.k.tnkms1127@gmail.com>
2023-06-18 11:11:42 +0900

    スライドショー完成

---
+++ js/outline.js
@@ -0,0 +1,56 @@
+const slide = document.querySelector(".js_slide");
+const go_left = document.querySelector(".go_left");
+const go_right = document.querySelector(".go_right");
+const loc = document.querySelectorAll(".loc span");
+const description = document.querySelector(".description");
+const width = window.innerWidth;
+
+//写真の一覧（画像のサイズはそろえること！！！）
+const img_src = ["img/dummyHotel.jpg", "img/dummyHotelRoom.jpg", "img/dummyHotelRoom2.jpg"];
+const img_alt = ["外観", "男子部屋", "女子部屋"];
+
+//写真の幅を調整
+slide.style.width = width - 20 + "px";
+
+//初期状態
+loc[0].style.backgroundColor = "#808080";
+
+//クリック時の操作
+let img_num = 0;
+function minus(num) {
+    num--;
+    if (num < 0) {
+        return img_src.length - 1;
+    }
+    return num;
+};
+
+function plus(num) {
+    num++;
+    if (img_src.length <= num) {
+        return 0;
+    }
+    return num;
+};
+
+go_right.addEventListener("click", () => {
+    //色をもとに戻す
+    loc[img_num].style.backgroundColor = "#dcdcdc";
+    //画像の番号をずらす
+    img_num = minus(img_num);
+    //新しいものに変更
+    loc[img_num].style.backgroundColor = "#808080";
+    description.textContent = img_alt[img_num];
+    slide.src = img_src[img_num];
+});
+
+go_left.addEventListener("click", () => {
+    //色をもとに戻す
+    loc[img_num].style.backgroundColor = "#dcdcdc";
+    //画像の番号をずらす
+    img_num = plus(img_num);
+    //新しいものに変更
+    loc[img_num].style.backgroundColor = "#808080";
+    description.textContent = img_alt[img_num];
+    slide.src = img_src[img_num];
+});
